02bb5f964ae9213aeadbc5d6549e992197cfbdbf
fajartc02 <fajartc02@gmail.com>
2021-10-28 09:00:30 +0700

    first

---
+++ Server/middleware/controllerUpload.js
@@ -0,0 +1,55 @@
+const multer = require('multer');
+const path = require('path');
+const dbMultiQuery = require('../config/MultipleQueryConnection');
+
+module.exports = {
+    uploadFile: (req, res, next) => {
+        console.log(req.query);
+        const storage = multer.diskStorage({
+            destination: `./upload/${req.query.folder}`,
+            // destination: function(req, file, cb) {
+            //     cb(null, `./upload/${req.query.folder}`);
+            // },
+
+            // // By default, multer removes file extensions so let's add them back
+            filename: function(req, file, cb) {
+                console.log(new Date().toISOString());
+                cb(null, req.query.nameFile + '-' + new Date().toISOString().split('T')[0] + path.extname(file.originalname));
+            }
+        });
+
+        // Files name is inside functions single
+        let upload = multer({ storage: storage}).single('file');
+        upload(req, res, function(err) {
+                // req.file contains information of uploaded file
+                // req.body contains information of text fields, if there were any
+                if (req.fileValidationError) {
+                    return res.status(500).json({
+                        message: 'Err',
+                        err: req.fileValidationError
+                    })
+                }
+                else if (!req.file) {
+                    return res.status(500).json({
+                        message: 'Err',
+                        err: 'Please select File'
+                    })
+                }
+                else if (err instanceof multer.MulterError) {
+                    return res.send(err);
+                }
+                else if (err) {
+                    return res.send(err);
+                }
+                // let q = `UPDATE tb_error_log_2 set fimage `
+                // dbMultiQuery()
+                // Display uploaded image for user validation
+                res.status(201).json({
+                    message: 'Success',
+                    path: req.file.destination + `/${req.file.filename}` 
+                })
+                
+                // res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+            });
+    }
+}
---
+++ Server/routes/index.js
@@ -0,0 +1,194 @@
+var express = require('express');
+var router = express.Router();
+const {
+    getColorDash,
+    getLines,
+    getMachines,
+    getProblemsToday,
+    getProblemsHistory,
+    getOeeLog,
+    getTotalDailyProb,
+    getParetoProblem,
+    getProbTemp,
+    searchMachine,
+    addProblem,
+    getDetailProblem,
+    editProblem,
+    deleteProblem,
+    getActiveProblem,
+    poolEnd
+} = require('../controllers/controllerProdAchievements')
+
+const {
+    postHenkaten,
+    getHenkaten,
+    deleteHenkaten,
+    editHenkaten
+} = require('../controllers/controllerHenkaten')
+
+const {
+    getAllMtMember,
+    signin,
+    signup
+} = require('../controllers/controllerMtMember')
+
+const {
+    addNewMachine,
+    mapMachines
+} = require('../controllers/controllerMachine')
+
+const {
+    getCmFollowup,
+    updateCmFinished
+} = require('../controllers/controllerCmFollowup')
+
+const {
+    getDataPareto
+} = require('../controllers/controllerRealtimePareto')
+
+const {
+    addQcData,
+    getAllQcData
+} = require('../controllers/controllerQcMonitor')
+
+const {
+    sendNotifWhatsapp,
+    sendWhatsapp
+} = require('../controllers/controllerNotif')
+
+const {
+    getTemperature,
+    getParameterName,
+    getAvailableMc,
+    getInverterData
+} = require('../controllers/controllerParameters')
+
+const { getAllCtMachines, getCtMcDashboard, getOneCtMachine } = require('../controllers/controllerCtMachine')
+
+const {
+    getProblemTemporary,
+    getProblemFreq,
+    getProblemLtb,
+    getSummaryWeekly
+} = require("../controllers/controllerProblem")
+
+const {uploadFile} = require('../middleware/controllerUpload')
+
+
+const fs = require('fs')
+const request = require('request')
+const stream = require('stream')
+
+router.get('/image', async (req, res) => {
+    const r = fs.createReadStream('./TEST.png') // or any other way to get a readable stream
+    const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
+    stream.pipeline(
+    r,
+    ps, // <---- this makes a trick with stream error handling
+    (err) => {
+        if (err) {
+        console.log(err) // No such file or any other kind of error
+        return res.sendStatus(400); 
+        }
+    })
+    ps.pipe(res) // <---- this makes a trick with stream error handling
+  });
+router.get('/', function(req, res) {
+    // // res.setHeader('Content-Type', 'application/pdf')
+    // // res.setHeader('Content-Disposition', 'inline;filename=doc1.pdf')
+    // // res.send()
+    // var data = fs.readFileSync('./doc1.pdf');
+    // res.contentType("application/pdf");
+    // res.send(data);
+
+    // var file = fs.createReadStream('./documents/doc1.pdf');
+    // var stat = fs.statSync('./documents/doc1.pdf');
+    // res.setHeader('Content-Length', stat.size);
+    // res.setHeader('Content-Type', 'application/pdf');
+    // // res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+    // file.pipe(res);
+
+    const path = './documents/doc1.pdf'
+    if (fs.existsSync(path)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(path).pipe(res)
+    } else {
+        res.status(500)
+        // console.log('File not found')
+        res.send('File not found')
+    }
+})
+router.post('/upload', uploadFile)
+
+
+// SUMMARY
+router.get('/delayProblemCm', getSummaryWeekly)
+    // CYCLE TIME MACHINE
+router.get('/ctMachines', getAllCtMachines)
+router.get('/ctMachinesDashboard', getCtMcDashboard)
+router.get('/ctOneMachine', getOneCtMachine)
+
+// QC DATA
+router.post('/addQcData', addQcData)
+router.get('/getAllQcData', getAllQcData)
+
+// SIGNIN
+router.get('/signin', signin)
+router.post('/signup', signup)
+
+/* GET home page. */
+router.get('/colordash', getColorDash);
+router.get('/lines', getLines);
+router.get('/searchMc', searchMachine);
+router.get('/countTempProb', getProbTemp);
+router.get('/machines', getMachines);
+router.get('/problemsToday', getProblemsToday);
+router.get('/problemHistory', getProblemsHistory)
+router.get('/oeeLog', getOeeLog)
+router.get('/getTotalDailyProb', getTotalDailyProb)
+router.get('/paretoProblem', getParetoProblem)
+router.get('/activeProblem', getActiveProblem)
+router.get('/problemTemporary', getProblemTemporary)
+router.get('/problemFreq', getProblemFreq)
+router.get('/problemLtb', getProblemLtb)
+
+// NOTIF
+router.get('/sendNotifWhatsapp', sendNotifWhatsapp)
+router.post('/sendWhatsapp', sendWhatsapp)
+
+// CONTERMEASURE
+router.get('/cmFollowup', getCmFollowup)
+router.post('/updateCmFinished', updateCmFinished)
+
+// TEMPERATURE
+router.get('/temperature', getTemperature)
+router.get('/paramsName', getParameterName)
+router.get('/availableParamMc', getAvailableMc)
+router.get('/inverterData', getInverterData)
+
+router.post('/addProblem', addProblem)
+router.get('/getDetailProblem', getDetailProblem)
+
+router.put('/editProblem/:v_', editProblem)
+router.delete('/deleteProblem/:v_', deleteProblem)
+
+router.get('/getHenkaten', getHenkaten)
+router.post('/postHenkaten', postHenkaten)
+router.delete('/deleteHenkaten/:v_', deleteHenkaten)
+router.put('/editHenkaten/:v_', editHenkaten)
+
+router.get('/getDataPareto', getDataPareto)
+
+router.get('/mapMachines', mapMachines)
+router.post('/addNewMachine', addNewMachine)
+
+router.get('/poolEnd', poolEnd)
+
+
+const pca = require('./pca')
+const parameter = require('./parameterRoute')
+
+router.use('/pca', pca)
+router.use('/parameter', parameter)
+
+module.exports = router;
