afdbbd333c7da03c2dfde29f59e92b960949a54b
Fajar Tri Cahyono <38496417+fajartc02@users.noreply.github.com>
2021-11-22 13:05:03 +0700

    setup limit express to 50mb, issue only 1mb limit upload by default

--- Server/app.js
+++ Server/app.js
@@ -14,8 +14,8 @@ var app = express();
 
 app.use(cors({ origin: true }));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
