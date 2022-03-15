44cd7cb33d131d9701b14d6dab9a89b89bf880bd
fajar cahyono <fajarcahyono112021@gmail.com>
2022-03-15 21:19:30 +0700

    update bug

--- Server/controllers/oee/controllerOee.js
+++ Server/controllers/oee/controllerOee.js
@@ -4,11 +4,11 @@ const cmdMultipleQuery = require('../../config/MultipleQueryConnection')
 module.exports = {
     getOeeAllLines: (req, res) => {
         let qOeeLinesOfMonth = `SELECT 
-            fline, 
+            UPPER(fline) as fline, 
             CAST(AVG(t.fvalue / 10) AS DECIMAL(7,1) ) as favg_oee, 
             fdate FROM (
                 SELECT 
-                    fline, 
+                    UPPER(fline) as fline, 
                     fvalue, 
                     fshift, 
                     fdate 
@@ -16,12 +16,17 @@ module.exports = {
                     tb_oee_log 
                 WHERE fshift = 1 
                     UNION 
-                SELECT fline, fvalue, fshift, fdate FROM 
+                SELECT UPPER(fline) as fline, fvalue, fshift, fdate FROM 
                 tb_oee_log WHERE fshift = 2) t 
             WHERE MONTH(fdate) = MONTH(NOW()) AND year(fdate) = year(now()) group by fdate,fline order by fdate ASC`
-        let q2 = `SELECT fid, fline, fmc_name, fstart_time, fdur, fshift, ferror_name
-FROM v_current_error_2 
-WHERE fline IN ('LPDC', 'HPDC', 'Crank Shaft', 'Cam Shaft', 'Cylinder Head', 'Cylinder Block', 'ASSY LINE') AND YEAR(fstart_time) = YEAR(NOW()) AND MONTH(fstart_time) = MONTH(NOW()) ORDER BY fdur DESC`
+        let q2 = `SELECT UPPER(fline) as fline, date(fstart_time) as fstart_time, SUM(fdur) as fdur
+from v_current_error_2
+WHERE MONTH(fstart_time) = MONTH(NOW()) AND year(fstart_time) = year(now())
+GROUP BY date(fstart_time),fline
+order by fstart_time ASC;`
+            //         let q2 = `SELECT fid, fline, fmc_name, fstart_time, fdur, fshift, ferror_name
+            // FROM v_current_error_2 
+            // WHERE fline IN ('LPDC', 'HPDC', 'Crank Shaft', 'Cam Shaft', 'Cylinder Head', 'Cylinder Block', 'ASSY LINE') AND YEAR(fstart_time) = YEAR(NOW()) AND MONTH(fstart_time) = MONTH(NOW()) ORDER BY fdur DESC`
         cmdMultipleQuery(`${qOeeLinesOfMonth};${q2}`)
             .then((result) => {
                 var groupResults = result[0].reduce(function(r, a) {
@@ -36,20 +41,13 @@ WHERE fline IN ('LPDC', 'HPDC', 'Crank Shaft', 'Cam Shaft', 'Cylinder Head', 'Cy
                 }, Object.create(null))
                 console.log(groupResultsProb);
                 let arrRes = []
-
-                function getSum(total, num) {
-                    return total + Math.round(num);
-                }
                 for (let key in groupResults) {
                     console.log(groupResults[key]);
-                    let mapResDur = groupResultsProb[key].map(item => {
-                        return item.fdur
-                    })
                     arrRes.push({
                         name: key,
                         dataOee: groupResults[key],
-                        dataProblem: groupResultsProb[key].splice(0, 2),
-                        dataSumDur: mapResDur.reduce(getSum, 0)
+                        // dataProblem: groupResultsProb[key].splice(0, 2),
+                        dataSumDur: groupResultsProb[key]
                     })
                 }
                 res.status(200).json({
@@ -63,5 +61,8 @@ WHERE fline IN ('LPDC', 'HPDC', 'Crank Shaft', 'Cam Shaft', 'Cylinder Head', 'Cy
                     err: err.message
                 })
             });
+    },
+    insertOeeLine: (req, res) => {
+        let q = `INSERT INTO tb_oee_log`
     }
 }
