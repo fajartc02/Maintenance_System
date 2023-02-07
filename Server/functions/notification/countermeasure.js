e41f8e4afb887adf424be69ddb29587f89062e59
fajar cahyono <fajarcahyono112021@gmail.com>
2023-02-07 09:33:01 +0700

    add feature for remind pic

--- Server/functions/notification/countermeasure.js
+++ Server/functions/notification/countermeasure.js
@@ -3,6 +3,15 @@ const formatDate = require('../../functions/formatDate')
 
 const configWa = require('./configWa')
 
+function getUser(name) {
+    return cmdMultipleQuery(`SELECT fname, fnoreg, fwa_no FROM tb_mt_member WHERE fname = '${name}'`)
+        .then((result) => {
+            return result
+        }).catch((err) => {
+            return err
+        });
+}
+
 module.exports = {
     getAllCountermeasure: (getNotifLeader) => {
         let qGetCmTerjadi = `select fid, fstart_time, fshift,ferror_name, line_id, fline, fmc_name, fpermanet_cm from v_current_error_2 where fpermanet_cm LIKE '%[{%' AND fstart_time BETWEEN '${formatDate.YYYYMMDD(new Date().getTime() - 1000 * 60 * 60 * 24 * 90)}' AND '${formatDate.YYYYMMDD(new Date().getTime())}' AND ferror_name LIKE "%[TASKFORCE]%"`
@@ -27,19 +36,39 @@ module.exports = {
                     let msgHeaderTemplate = `*SMART COUNTERMEASURE REMINDER*\n`
                     let msgInfoTemplate = `Line: ${itm.item.fline}\nMc: ${itm.item.fmc_name}\nProblem:${itm.item.ferror_name}\n\n`
                     let msgLink = `\n\nUntuk Detail:\n https://smartandonsys.web.app/editProblem?v_=${itm.item.fid}`
-                    let container = itm.parse.map((cm, i) => {
+                    let container = itm.parse.map(async (cm, i) => {
                         let status;
                         let days = Math.floor((new Date(cm.datePlan).getTime() - new Date().getTime()) / 1000 / 60 / 60 / 24)
                         console.log(days);
                         if (days <= 3 && days >= 0) {
                             // H-3 until H
                             status = `Ayo Prepare H-${days + 1} Deadline`
+                            let userData = await getUser(cm.pic)
+                            console.log(userData);
+                            if (userData.length > 0) {
+                                msgUser = `Hallo pak/bu ${userData[0].fname}(${userData[0].fnoreg}), Ayo Prepare Penganggulangan: \n\n(${i + 1}) Desc:${cm.cmDesc}\nPlan:${cm.datePlan}\nPIC:${cm.pic}\nStatus:${status} \n\n klik link untuk detail:\nhttps://smartandonsys.web.app/editProblem?v_=${itm.item.fid}`
+                                configWa(msgHeaderTemplate + msgInfoTemplate + msgUser, userData[0].fwa_no, 'CM INDIVIDU')
+                            }
                             return `(${i + 1}) Desc:${cm.cmDesc}\nPlan:${cm.datePlan}\nPIC:${cm.pic}\nStatus:${status}`
                         } else if (days == -1) {
                             status = `Hari Ini Deadline`
+                            let userData = await getUser(cm.pic)
+                            console.log(userData);
+                            if (userData.length > 0) {
+                                msgUser = `Hallo pak/bu ${userData[0].fname}(${userData[0].fnoreg}), Silahkan Melakukan Penganggulangan: \n\n(${i + 1}) Desc:${cm.cmDesc}\nPlan:${cm.datePlan}\nPIC:${cm.pic}\nStatus:${status} \n\n klik link untuk detail:\nhttps://smartandonsys.web.app/editProblem?v_=${itm.item.fid}`
+                                configWa(msgHeaderTemplate + msgInfoTemplate + msgUser, userData[0].fwa_no, 'CM INDIVIDU')
+
+                            }
                             return `(${i + 1}) Desc:${cm.cmDesc}\nPlan:${cm.datePlan}\nPIC:${cm.pic}\nStatus:${status}`
                         } else if (days < -3) {
-                            status = `*Delay ${days * -1} Hari*`
+                            status = `*Delay ${(days + 1) * -1} Hari*`
+                            let userData = await getUser(cm.pic)
+                            console.log(userData);
+                            if (userData.length > 0) {
+                                msgUser = `Hallo pak/bu ${userData[0].fname}(${userData[0].fnoreg}), Silahkan Melakukan Penganggulangan: \n\n(${i + 1}) Desc:${cm.cmDesc}\nPlan:${cm.datePlan}\nPIC:${cm.pic}\nStatus:${status} \n\n klik link untuk detail:\nhttps://smartandonsys.web.app/editProblem?v_=${itm.item.fid}`
+
+                                configWa(msgHeaderTemplate + msgInfoTemplate + msgUser, userData[0].fwa_no, 'CM INDIVIDU')
+                            }
                             return `(${i + 1}) Desc:${cm.cmDesc}\nPlan:${cm.datePlan}\nPIC:${cm.pic}\nStatus:${status}`
                         }
                     })
