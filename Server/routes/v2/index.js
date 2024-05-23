6242a06abbebe6be8517ce08f8b56d5ddf20d90c
fajartc02 <fajartc02@gmail.com>
2024-05-24 01:18:06 +0700

    update Q6 Analysis

--- Server/routes/v2/index.js
+++ Server/routes/v2/index.js
@@ -2,8 +2,12 @@ var router = require("express").Router();
 
 const problemRoute = require("./problemRoute");
 const { getLtbHistory } = require("../../controllers/v2/LTBHistory");
+const {
+  getGraphQ6,
+} = require("../../controllers/v2/Q6/q6_anlysis.controllers");
 
 router.get("/ltb-history", getLtbHistory);
+router.get("/q6-analysis/graph", getGraphQ6);
 
 router.use("/master", problemRoute);
 
