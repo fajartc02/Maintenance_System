b7a69232c0ec98b29ea7febcc4236c9f81542127
fajartc02 <fajartc02@gmail.com>
2024-06-24 14:09:38 +0700

    update

--- Server/controllers/analisys-new/analisys.js
+++ Server/controllers/analisys-new/analisys.js
@@ -1,43 +1,56 @@
-const cmdMultipleQuery = require('../../config/MultipleQueryConnection');
-
+const cmdMultipleQuery = require("../../config/MultipleQueryConnection");
 
 module.exports = {
     addAnalisysNew: async(req, res) => {
         try {
-            const data = await cmdMultipleQuery(`SELECT id from o_analisys where id_problem = ${req.params.v_} AND analisys_category = '${req.query.analisys_category}'`)
-            console.log(data)
+            const data = await cmdMultipleQuery(
+                `SELECT id from o_analisys where id_problem = ${req.params.v_} AND analisys_category = '${req.query.analisys_category}'`
+            );
+
             if (data.length > 0) {
-                await cmdMultipleQuery(`UPDATE o_analisys SET json_string = '${JSON.stringify(req.body)}' where id_problem = ${req.params.v_} AND analisys_category = '${req.query.analisys_category}'`)
+                await cmdMultipleQuery(
+                    `UPDATE o_analisys SET json_string = '${JSON.stringify(
+            req.body
+          )}' where id_problem = ${req.params.v_} AND analisys_category = '${
+            req.query.analisys_category
+          }'`
+                );
                 return res.status(200).json({
-                    message: 'success update',
-                })
+                    message: "success update",
+                });
             } else {
-                await cmdMultipleQuery(`INSERT INTO o_analisys(id_problem, json_string, analisys_category) VALUES (${req.params.v_}, '${JSON.stringify(req.body)}', '${req.query.analisys_category}')`)
+                await cmdMultipleQuery(
+                    `INSERT INTO o_analisys(id_problem, json_string, analisys_category) VALUES (${
+            req.params.v_
+          }, '${JSON.stringify(req.body)}', '${req.query.analisys_category}')`
+                );
                 return res.status(200).json({
-                    message: 'success insert',
-                })
+                    message: "success insert",
+                });
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
             return res.status(500).json({
-                message: 'Error add analysis',
-                error
-            })
+                message: "Error add analysis",
+                error,
+            });
         }
     },
     getAnalisysNew: async(req, res) => {
         try {
-            const data = await cmdMultipleQuery(`SELECT json_string from o_analisys where id_problem = ${req.params.v_} AND analisys_category = '${req.query.analisys_category}'`)
+            const data = await cmdMultipleQuery(
+                `SELECT json_string from o_analisys where id_problem = ${req.params.v_} AND analisys_category = '${req.query.analisys_category}'`
+            );
             return res.status(200).json({
-                message: 'success',
-                data
-            })
+                message: "success",
+                data,
+            });
         } catch (error) {
-            console.log(error)
+            console.log(error);
             return res.status(500).json({
-                message: 'Error',
-                err: error
-            })
+                message: "Error",
+                err: error,
+            });
         }
-    }
-}
+    },
+};
